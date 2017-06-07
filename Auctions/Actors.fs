2396d54d00b7eea155b1b1daf2de8b60a3de3e1e
Oskar Gewalli <oskar@gewalli.se>
2017-06-07 08:01:02 +0200

    Reasoning

---
+++ Auctions/Actors.fs
@@ -0,0 +1,67 @@
+﻿module Auctions.Actors
+
+open System.Threading
+open System.Threading.Tasks
+open Commands
+open Domain
+(*
+Calculate running auctions : init agents
+
+signal -> delegator (calculate x) -> agents.[x] 
+
+signal start auction -> 
+                        agent.Start()
+                        agents <- agent :: agents 
+singal end auction ->
+                        agent.Stop()
+                        agents <- agents |> List.except [agent]
+
+
+command    -> agents.[x] --> Maybe error
+                         \
+                          \-> post auction update to clients
+                          |
+                          \-> 
+
+Assumptions:
+
+- each auction get 1 agent 
+- all commands are logged in json et.c. 
+- one dedicated thread per command persister
+
+
+query      -> agents.[x] --> Result<QueryResult,QueryError>
+
+*)
+
+type Agent<'T> = MailboxProcessor<'T>
+
+let createAgent (errorQueue : MailboxProcessor<Error>) = 
+  Agent<Command* AsyncReplyChannel<Error option>>.Start(fun inbox -> 
+    (let r = Repository()
+     
+     let rec messageLoop() = 
+       async { 
+         let! (msg,reply) = inbox.Receive()
+         let maybeError = handleCommand r msg
+         match maybeError with
+         | Ok() -> ()
+         | Error e -> 
+             reply.Reply (Some e)
+             errorQueue.Post e
+
+         return! messageLoop()
+       }
+     messageLoop()))
+
+let createErrorListener() = 
+  Agent<Error>.Start(fun inbox -> 
+    (let rec messageLoop() = 
+       async { 
+         let! msg = inbox.Receive()
+         printfn "%A" msg
+         return! messageLoop()
+       }
+     messageLoop()))
+
+
