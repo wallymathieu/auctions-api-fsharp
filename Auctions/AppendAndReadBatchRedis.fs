fce3ed822c4ef4f53f9ef1d87a88a4a32857f100
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2018-09-02 21:15:30 +0200

    Codec from Fleece (#14)
    * Codec from Fleece
    
    * Fixes after code review by @gusty

--- Auctions/AppendAndReadBatchRedis.fs
+++ Auctions/AppendAndReadBatchRedis.fs
@@ -5,6 +5,7 @@ open Auctions.MapToRedis
 open StackExchange.Redis
 open System.Collections.Generic
 open System
+open FSharpPlus
 
 type AppendAndReadBatchRedis(connStr:string) = 
   let conn = ConnectionMultiplexer.Connect(connStr)
@@ -12,10 +13,10 @@ type AppendAndReadBatchRedis(connStr:string) =
   let hashCreate (batch : IBatch) command = 
     let id = Guid.NewGuid().ToString("N")
     let entries = mapToHashEntries command
-    batch.HashSetAsync(redisKey id, entries |> List.toArray) |> ignore
+    batch.HashSetAsync(implicit id, entries |> List.toArray) |> ignore
     id
   
-  let commandsKey = redisKey "Commands"
+  let commandsKey = implicit "Commands"
   interface IAppendBatch with
     
     member __.Batch commands = 
@@ -23,13 +24,13 @@ type AppendAndReadBatchRedis(connStr:string) =
       let ids = new List<RedisValue>()
       for command in commands do
         let id = hashCreate batch command
-        ids.Add(redisValueStr (string id))
+        ids.Add(implicit (string id))
       batch.SetAddAsync(commandsKey, ids |> Seq.toArray, CommandFlags.None) |> ignore
       batch.Execute()
     
     member __.ReadAll() = 
       let commands = db.SetMembers(commandsKey, CommandFlags.None)
       commands
-      |> Array.map( (db.HashGetAll<<valueToKey) >>mapFromHashEntries )
+      |> Array.map( (db.HashGetAll<<(string>>implicit)) >> List.ofArray >> mapFromHashEntries )
       |> Array.toList
       |> List.sortBy Command.getAt
