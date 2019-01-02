144da1d77d8eb737ff0d26d9d17cc0bbefe69ab5
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-01-02 12:00:54 +0100

    Cleanup after merge

--- Auctions/Giraffe.fs
+++ Auctions/Giraffe.fs
@@ -3,13 +3,7 @@ open Giraffe
 open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks.V2
 open System.IO
-open Fleece.FSharpData
-let inline json dataObj : HttpHandler =
-    fun (next : HttpFunc) (ctx : HttpContext) ->
-        ctx.SetContentType "application/json; charset=utf-8"
-        toJson dataObj
-        |> string
-        |> ctx.WriteStringAsync
+
 module Json =
   open FSharp.Data
   let inline ``OK_or_BAD_REQUEST`` (result:Result<_,_>) = fun next ctx ->
@@ -21,10 +15,4 @@ module Json =
     use reader = new StreamReader(ctx.Request.Body)
     let! body = reader.ReadToEndAsync()
     return try Ok (JsonValue.Parse body) with e -> Error <| string e
-  }
-
-  let inline parseBody (ctx : HttpContext) = task {
-    use reader = new StreamReader(ctx.Request.Body)
-    let! body = reader.ReadToEndAsync()
-    return parseJson(body)
   }
