3b5119f1531f62514a7c34ce1d3669c770618ef0
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-12-31 22:12:47 +0100

    +Props

--- Tests/TestsModule.fs
+++ Tests/TestsModule.fs
@@ -3,6 +3,8 @@ module Tests.TestsModule
 open Auctions.Domain
 open System
 open FSharpPlus
+open FsCheck
+open System.Text.RegularExpressions
 
 let auctionId = AuctionId 1L
 let title = "auction"
@@ -58,3 +60,31 @@ let addBidsToState state=
 let tee on something=
   on something
   something
+
+
+module Arb =
+    let nonNullAndNotNewlineOrSpace =
+        Arb.from<string>
+        |> Arb.filter (fun s -> s <> null && Regex.IsMatch (s, "^[^\n\r\t ]+$"))
+    let nonNullAndNotVerticalBarOrNewLineOrSpace =
+        nonNullAndNotNewlineOrSpace |> Arb.filter (fun s -> Regex.IsMatch (s, "^[^|]+$"))
+
+
+type NonNullAndNotVerticalBarOrNewLineOrSpace = NonNullAndNotVerticalBarOrNewLineOrSpace of string with
+  static member op_Explicit(NonNullAndNotVerticalBarOrNewLineOrSpace i) = i
+type NonNullAndNotNewlineOrSpace = NonNullAndNotNewlineOrSpace of string with
+  static member op_Explicit(NonNullAndNotNewlineOrSpace i) = i
+
+type ArbitraryModifiers =
+    static member NonNullAndNotVerticalBarOrNewLineOrSpace() =
+        Arb.nonNullAndNotVerticalBarOrNewLineOrSpace
+        |> Arb.convert NonNullAndNotVerticalBarOrNewLineOrSpace string
+    static member NonNullAndNotNewlineOrSpace() =
+        Arb.nonNullAndNotNewlineOrSpace
+        |> Arb.convert NonNullAndNotNewlineOrSpace string
+
+Arb.register<ArbitraryModifiers>() |> ignore
+
+let inline (?=?) left right = left = right |@ sprintf "%A = %A" left right
+
+let fsCheck s x = Check.One({Config.QuickThrowOnFailure with Name = s}, x)
--- Tests/UserTests.fs
+++ Tests/UserTests.fs
@@ -2,6 +2,7 @@
 open Auctions.Domain
 open System
 open Xunit
+open FsCheck
 
 type ``Can parse user``() =
 
@@ -14,3 +15,18 @@ type ``Can parse user``() =
   member test.Support() =
     let user = Guid.NewGuid().ToString("N") |> UserId |> Support
     Assert.Equal(Some user, string user |> User.TryParse)
+
+  [<Fact>]
+  member test.``Support roundtrip``() =
+    let roundtrip (u) =
+      let orig = UserId u |> Support
+      let parsed = orig |> string |> User.TryParse
+      Some orig ?=? parsed
+    fsCheck "roundtrip" (Prop.forAll Arb.nonNullAndNotVerticalBarOrNewLineOrSpace roundtrip)
+  [<Fact>]
+  member test.``Buyer or seller roundtrip``() =
+    let roundtrip (NonNullAndNotVerticalBarOrNewLineOrSpace u) (NonNullAndNotNewlineOrSpace name)=
+      let orig = BuyerOrSeller (UserId u, name)
+      let parsed = orig |> string |> User.TryParse
+      Some orig ?=? parsed
+    fsCheck "roundtrip" roundtrip
